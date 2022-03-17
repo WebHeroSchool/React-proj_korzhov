28334fef28a299a91fc3b57a2eb3d0925d192227
KorzhovVD <svalesik@mail.ru>
2022-03-17 16:57:23 +0300

    Update Item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -9,34 +9,51 @@ import ListItem from '@mui/material/ListItem';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 
-const Item = ({value, isDone, onClickDone, id, onClickDelete, item}) => (
-	<div>
-		<ListItem>
-			<ListItemIcon>
-	           	<Checkbox 
-	       			color="primary" 
-	       			inputProps={{'aria-label': 'uncontrolled-checkbox'}} 
-	       			onClick = {() => onClickDone(id)} 
-	       		/>
-	        </ListItemIcon>
+class Item extends React.Component {
+	componentDidMount() {
+	    console.log('componentDidMount');
+	}
 
-	        <ListItemText> 
-				<span className = {
-					classnames ({
-						[styles.item]: true,
-						[styles.done]: isDone
-					})
-				}>
-					{value}
-				</span>;
-	        </ListItemText>
+	componentDidUpdate() {
+	    console.log('componentDidUpdate');
+	}
 
-			<IconButton aria-label="delete" onClick = {() => onClickDelete(id)} >
-				<DeleteIcon />
-			</IconButton>
-		</ListItem>
-	</div>);
-	
+	componentWillUnmount() {
+	    console.log('componentWillUnmount');
+	}	
+
+	render(){
+		const {value, isDone, onClickDone, id, onClickDelete, item} = this.props;
+		return(
+			<div>
+				<ListItem>
+					<ListItemIcon>
+			           	<Checkbox 
+			       			color="primary" 
+			       			inputProps={{'aria-label': 'uncontrolled-checkbox'}} 
+			       			onClick = {() => onClickDone(id)} 
+			       		/>
+			        </ListItemIcon>
+
+			        <ListItemText> 
+						<span className = {
+							classnames ({
+								[styles.item]: true,
+								[styles.done]: isDone
+							})
+						}>
+							{value}
+						</span>;
+			        </ListItemText>
+
+					<IconButton aria-label="delete" onClick = {() => onClickDelete(id)} >
+						<DeleteIcon />
+					</IconButton>
+				</ListItem>
+			</div>);
+	}
+}
+
 Item.propTypes = {
 	isDone: PropTypes.bool,
 	value: PropTypes.string,
